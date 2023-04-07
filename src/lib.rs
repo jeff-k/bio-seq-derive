66dde41fc6bfc37b5f6ee2fe0ee888cd25ecb1af
jeff-k <jeff_k@fastmail.com>
2023-04-07 23:35:04 +0100

    further cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ struct WidthAttr {
     width: syn::LitInt,
 }
 
+/// The width attribute should take the form #[width = 2]
 impl syn::parse::Parse for WidthAttr {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let _: syn::Token![=] = input.parse()?;
@@ -19,23 +20,11 @@ impl syn::parse::Parse for WidthAttr {
     }
 }
 
-fn codec_width(attrs: &[syn::Attribute]) -> u8 {
-    let mut width: u8 = 4;
-    for attr in attrs {
-        if attr.path.is_ident("width") {
-            width = match syn::parse2::<WidthAttr>(attr.tokens.clone()) {
-                Ok(w) => w.width.base10_parse::<u8>().unwrap(),
-                _ => 4,
-            };
-        };
-    }
-    width
-}
-
 struct AltAttr {
     chr: syn::LitChar,
 }
 
+/// Alternate representation attributes look like #[alt(0b00, 0b11)]
 impl syn::parse::Parse for AltAttr {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let _: syn::Token![=] = input.parse()?;
@@ -44,37 +33,12 @@ impl syn::parse::Parse for AltAttr {
     }
 }
 
-/// Parse alternative representations
-fn codec_altrepr(attrs: &[syn::Attribute]) -> Option<char> {
-    for attr in attrs {
-        if attr.path.is_ident("display") {
-            return match syn::parse2::<AltAttr>(attr.tokens.clone()) {
-                Ok(c) => Some(c.chr.value()),
-                _ => panic!("expecting char in 'display' attribute"),
-            };
-        };
-    }
-    None
-}
-
-/// Parse alternative discriminants
-fn codec_altdisc(attrs: &[syn::Attribute]) -> Option<Vec<syn::ExprLit>> {
-    for attr in attrs {
-        if attr.path.is_ident("alt") {
-            let discs: Punctuated<syn::ExprLit, Token![,]> =
-                attr.parse_args_with(Punctuated::parse_terminated).unwrap();
-            return Some(discs.iter().cloned().collect());
-        };
-    }
-    None
-}
-
 #[proc_macro_derive(Codec, attributes(width, display, alt))]
 pub fn codec_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as syn::Item);
 
-    // Basic test for correct usage
-    let e = match input {
+    // Test for correct usage
+    let enum_ast = match input {
         syn::Item::Enum(e) => e,
         _ => {
             return syn::Error::new_spanned(input, "Codec can only be derived for enums")
@@ -83,11 +47,9 @@ pub fn codec_derive(input: TokenStream) -> TokenStream {
         }
     };
 
-    let variants = e.variants;
-    let ident = e.ident;
-    let width: u8 = codec_width(&e.attrs);
-
-    let mut max_variant = 0u64;
+    let variants = enum_ast.variants;
+    let enum_ident = enum_ast.ident;
+    let mut max_variant = 0u8;
     let mut variant_idents = Vec::new();
 
     let mut variants_to_char = Vec::new();
@@ -95,19 +57,18 @@ pub fn codec_derive(input: TokenStream) -> TokenStream {
     let mut alt_discriminants = Vec::new();
     let mut unsafe_alts = Vec::new();
 
-    // Test that the maximum discriminant is less than the greatest amount
-    // supported by the bitwidth and collect the list of identifiers.
     for variant in variants.iter() {
-        variant_idents.push(variant.ident.clone());
+        let ident = &variant.ident;
+        variant_idents.push(ident.clone());
         let discriminant = &variant.discriminant;
 
         if let Some((_, syn::Expr::Lit(expr_lit))) = discriminant {
             let value = match &expr_lit.lit {
-                syn::Lit::Byte(lit_byte) => lit_byte.value() as u64,
-                syn::Lit::Int(lit_int) => lit_int.base10_parse::<u64>().unwrap(),
+                syn::Lit::Byte(lit_byte) => lit_byte.value(),
+                syn::Lit::Int(lit_int) => lit_int.base10_parse::<u8>().unwrap(),
                 _ => {
                     return syn::Error::new_spanned(
-                        &variant.ident,
+                        ident,
                         "Codec derivations require byte or integer discriminants",
                     )
                     .to_compile_error()
@@ -115,50 +76,71 @@ pub fn codec_derive(input: TokenStream) -> TokenStream {
                 }
             };
 
+            alt_discriminants.push(quote! { #value => Ok(Self::#ident) });
+            unsafe_alts.push(quote! { #value => Self::#ident });
+
             max_variant = max_variant.max(value);
         } else {
-            return syn::Error::new_spanned(
-                &variant.ident,
-                "Codec derivations require discriminants",
-            )
-            .to_compile_error()
-            .into();
+            return syn::Error::new_spanned(ident, "Codec derivations require discriminants")
+                .to_compile_error()
+                .into();
         }
 
-        let ident = &variant.ident;
+        let mut char_repr = ident.to_string().chars().next().unwrap();
 
-        let alt_char = codec_altrepr(&variant.attrs);
-        let char_repr = alt_char.unwrap_or_else(|| ident.to_string().chars().next().unwrap());
+        for attr in &variant.attrs {
+            if attr.path.is_ident("display") {
+                let alt_attr: AltAttr = match syn::parse2(attr.tokens.clone()) {
+                    Ok(attr) => attr,
+                    Err(err) => return err.to_compile_error().into(),
+                };
+                char_repr = alt_attr.chr.value();
+            } else if attr.path.is_ident("alt") {
+                let discs: Punctuated<syn::ExprLit, Token![,]> =
+                    match attr.parse_args_with(Punctuated::parse_terminated) {
+                        Ok(discs) => discs,
+                        Err(err) => return err.to_compile_error().into(),
+                    };
+
+                for d in discs.into_iter() {
+                    alt_discriminants.push(quote! { #d => Ok(Self::#ident) });
+                    unsafe_alts.push(quote! { #d => Self::#ident });
+                }
+            };
+        }
 
         variants_to_char.push(quote! { Self::#ident => #char_repr });
         chars_to_variant.push(quote! { #char_repr => Ok(Self::#ident) });
-
-        match discriminant {
-            Some((_, d)) => {
-                alt_discriminants.push(quote! { #d => Ok(Self::#ident) });
-                unsafe_alts.push(quote! { #d => Self::#ident });
-            }
-            None => panic!(),
-        }
-
-        let alt_disc = codec_altdisc(&variant.attrs);
-        if let Some(ds) = alt_disc {
-            for d in ds {
-                alt_discriminants.push(quote! { #d => Ok(Self::#ident) });
-                unsafe_alts.push(quote! { #d => Self::#ident });
-            }
-        }
     }
 
-    if max_variant >= u64::pow(2, width as u32) {
-        return syn::Error::new_spanned(ident, "Codec specifier does not fit inside bit width")
-            .to_compile_error()
-            .into();
+    // TODO: we should be able to use the best fitting width if we know the max
+    // value of the variants, ceil(log2(max_variant)). This should be carefully
+    // tested.
+    //
+    //    let mut width = max_variant.ilog2();
+
+    let mut width = 8; // default width
+    for attr in &enum_ast.attrs {
+        if attr.path.is_ident("width") {
+            width = match syn::parse2::<WidthAttr>(attr.tokens.clone()) {
+                Ok(w) => w.width.base10_parse::<u8>().unwrap(),
+                Err(err) => return err.to_compile_error().into(),
+            }
+        };
+    }
+
+    if max_variant >= u8::pow(2, width as u32) {
+        return syn::Error::new_spanned(
+            enum_ident,
+            "Codec specifier does not fit inside bit width",
+        )
+        .to_compile_error()
+        .into();
     }
 
     // Generate the implementation
     let output = quote! {
-        impl Codec for #ident {
+        impl Codec for #enum_ident {
             type Error = ParseBioErr;
             const WIDTH: u8 = #width;
             fn unsafe_from_bits(b: u8) -> Self {
@@ -189,7 +171,7 @@ pub fn codec_derive(input: TokenStream) -> TokenStream {
             }
         }
 
-        impl #ident {
+        impl #enum_ident {
             pub fn iter() -> impl Iterator<Item = Self> {
                 vec![ #(Self::#variant_idents,)* ].into_iter()
             }
