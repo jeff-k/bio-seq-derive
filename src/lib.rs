130c77e2ddfd6e0d5410160360ac0b5c283daf70
jeff-k <jeff_k@fastmail.com>
2023-04-07 17:52:26 +0100

    added implementation for item iterator, better error messages

--- src/lib.rs
+++ src/lib.rs
@@ -85,166 +85,166 @@ fn codec_altdisc(attrs: &[syn::Attribute]) -> Option<Vec<syn::ExprLit>> {
 
 #[proc_macro_derive(Codec, attributes(width, display, alt))]
 pub fn codec_derive(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as syn::Item);
+    let input = parse_macro_input!(input as syn::Item);
 
-    match ast {
-        syn::Item::Enum(e) => {
-            let variants = e.variants;
-            let ident = e.ident;
-            let width: u8 = codec_width(&e.attrs);
+    let e = match input {
+        syn::Item::Enum(e) => e,
+        _ => {
+            return syn::Error::new_spanned(input, "Codec can only be derived for enums")
+                .to_compile_error()
+                .into()
+        }
+    };
 
-            // TODO
-            // We can check whether the max value of the variants fits into the
-            // specified bit width
-            // We want to run this check on the alternative variants as well
-            let max_variant = 255;
-            //let width: u8 = 4;
+    let variants = e.variants;
+    let ident = e.ident;
+    let width: u8 = codec_width(&e.attrs);
 
-            for v in variants.iter() {
-                //let ident = &v.ident;
-                let discriminant = &v.discriminant;
-                match discriminant {
-                    // get max value out of the discriminant Expr (should be LitByte() with value member)
-                    Some((_, _d)) => (),
-                    _ => panic!(),
-                    //None => return syn::Error::new_spanned(ident, "Codec derivations require discriminants").to_compile_error().into(),
-                }
-            }
+    let mut max_variant = 0u64;
 
-            if max_variant < u64::pow(2, width as u32) {
-                return syn::Error::new_spanned(
-                    ident,
-                    "Codec specifier does not fit inside bit width",
-                )
+    let mut variant_idents = vec![];
+    for v in variants.iter() {
+        //let ident = &v.ident;
+        variant_idents.push(v.ident.clone());
+        let discriminant = &v.discriminant;
+
+        if let Some((
+            _,
+            syn::Expr::Lit(syn::ExprLit {
+                lit: syn::Lit::Byte(lit_byte),
+                ..
+            }),
+        )) = discriminant
+        {
+            let value = lit_byte.value() as u64;
+            max_variant = max_variant.max(value);
+        } else {
+            return syn::Error::new_spanned(&v.ident, "Codec derivations require discriminants")
                 .to_compile_error()
                 .into();
-            }
-
-            let variants_to_char = variants.iter().map(|v| {
-                let ident = &v.ident;
-                let discriminant = &v.discriminant;
-
-                let alt_char = codec_altrepr(&v.attrs);
-
-                match alt_char {
-                    Some(alt) => quote! { Self::#ident => #alt },
-                    None => match discriminant {
-                        Some(_d) => {
-                            let char_repr: char = ident.to_string().chars().next().unwrap();
-                            quote! { Self::#ident => #char_repr }
-                        }
-                        None => panic!(),
-                    },
-                }
-            });
-            let chars_to_variant = variants.iter().map(|v| {
-                let ident = &v.ident;
-                let discriminant = &v.discriminant;
-
-                let alt_char = codec_altrepr(&v.attrs);
-
-                match alt_char {
-                    Some(alt) => quote! { #alt => Ok(Self::#ident) },
-                    None => match discriminant {
-                        Some(_d) => {
-                            let char_repr: char = ident.to_string().chars().next().unwrap();
-                            quote! { #char_repr => Ok(Self::#ident) }
-                        }
-                        None => panic!(),
-                    },
-                }
-            });
-
-            /*
-                        let bits_to_variant = variants.iter().map(|v| {
-                            let ident = &v.ident;
-                            let discriminant = &v.discriminant;
-
-                            match discriminant {
-                                Some((_, d)) => {
-                                    quote! { #d => Ok(Self::#ident) }
-                                }
-                                None => panic!(),
-                            }
-
-                        });
-            */
-            let mut altds = Vec::new();
-            let mut altds_unsafe = Vec::new();
-
-            for v in &variants {
-                let ident = &v.ident;
-
-                let discriminant = &v.discriminant;
-
-                match discriminant {
-                    Some((_, d)) => {
-                        altds.push(quote! { #d => Ok(Self::#ident) });
-                        altds_unsafe.push(quote! { #d => Self::#ident });
-                    }
-                    None => panic!(),
-                }
-
-                let alt_disc = codec_altdisc(&v.attrs);
-                match alt_disc {
-                    Some(ds) => {
-                        for d in ds {
-                            altds.push(quote! { #d => Ok(Self::#ident) });
-                            altds_unsafe.push(quote! { #d => Self::#ident });
-                        }
-                    }
-                    None => (),
-                }
-            }
-            /*
-                        let unsafe_bit_variants = variants.iter().map(|v| {
-                            let ident = &v.ident;
-                            let discriminant = &v.discriminant;
-
-                            match discriminant {
-                                Some((_, d)) => {
-                                    quote! { #d => Self::#ident }
-                                }
-                                None => panic!(),
-                            }
-                        });
-            */
-            let output = quote! {
-                            impl Codec for #ident {
-                                type Error = ParseBioErr;
-                                const WIDTH: u8 = #width;
-                                fn unsafe_from_bits(b: u8) -> Self {
-                                    match b {
-             //                           #(#unsafe_bit_variants),*
-                                        #(#altds_unsafe),*,
-                                        _ => panic!(),
-                                    }
-                                }
-
-                                fn try_from_bits(b: u8) -> Result<Self, ParseBioErr> {
-                                    match b {
-            //                            #(#bits_to_variant),*
-                                        #(#altds),*,
-                                        _ => Err(ParseBioErr),
-                                    }
-                                }
-
-                                fn from_char(c: char) -> Result<Self, ParseBioErr> {
-                                    match c {
-                                        #(#chars_to_variant),*,
-                                        _ => Err(ParseBioErr),
-                                    }
-                                }
-
-                                fn to_char(self) -> char {
-                                    match self {
-                                        #(#variants_to_char),*,
-                                    }
-                                }
-                            }
-                        };
-            output.into()
         }
-        _ => panic!("not enum"),
     }
+
+    if max_variant >= u64::pow(2, width as u32) {
+        return syn::Error::new_spanned(ident, "Codec specifier does not fit inside bit width")
+            .to_compile_error()
+            .into();
+    }
+
+    let variants_to_char = variants.iter().map(|v| {
+        let ident = &v.ident;
+        let discriminant = &v.discriminant;
+
+        let alt_char = codec_altrepr(&v.attrs);
+
+        match alt_char {
+            Some(alt) => quote! { Self::#ident => #alt },
+            None => match discriminant {
+                Some(_d) => {
+                    let char_repr: char = ident.to_string().chars().next().unwrap();
+                    quote! { Self::#ident => #char_repr }
+                }
+                None => {
+                    return syn::Error::new_spanned(
+                        ident,
+                        "Could not parse alternative representation",
+                    )
+                    .to_compile_error()
+                    .into()
+                }
+            },
+        }
+    });
+    let chars_to_variant = variants.iter().map(|v| {
+        let ident = &v.ident;
+        let discriminant = &v.discriminant;
+
+        let alt_char = codec_altrepr(&v.attrs);
+
+        match alt_char {
+            Some(alt) => quote! { #alt => Ok(Self::#ident) },
+            None => match discriminant {
+                Some(_d) => {
+                    let char_repr: char = ident.to_string().chars().next().unwrap();
+                    quote! { #char_repr => Ok(Self::#ident) }
+                }
+                None => {
+                    return syn::Error::new_spanned(
+                        ident,
+                        "Could not parse alternative representation",
+                    )
+                    .to_compile_error()
+                    .into()
+                }
+            },
+        }
+    });
+
+    let mut altds = Vec::new();
+    let mut altds_unsafe = Vec::new();
+
+    for v in &variants {
+        let ident = &v.ident;
+
+        let discriminant = &v.discriminant;
+
+        match discriminant {
+            Some((_, d)) => {
+                altds.push(quote! { #d => Ok(Self::#ident) });
+                altds_unsafe.push(quote! { #d => Self::#ident });
+            }
+            None => panic!(),
+        }
+
+        let alt_disc = codec_altdisc(&v.attrs);
+        match alt_disc {
+            Some(ds) => {
+                for d in ds {
+                    altds.push(quote! { #d => Ok(Self::#ident) });
+                    altds_unsafe.push(quote! { #d => Self::#ident });
+                }
+            }
+            None => (),
+        }
+    }
+
+    let output = quote! {
+        impl Codec for #ident {
+            type Error = ParseBioErr;
+            const WIDTH: u8 = #width;
+            fn unsafe_from_bits(b: u8) -> Self {
+                match b {
+                    #(#altds_unsafe),*,
+                    _ => panic!(),
+                }
+            }
+
+            fn try_from_bits(b: u8) -> Result<Self, ParseBioErr> {
+                match b {
+                    #(#altds),*,
+                    _ => Err(ParseBioErr),
+                }
+            }
+
+            fn from_char(c: char) -> Result<Self, ParseBioErr> {
+                match c {
+                    #(#chars_to_variant),*,
+                    _ => Err(ParseBioErr),
+                }
+            }
+
+            fn to_char(self) -> char {
+                match self {
+                    #(#variants_to_char),*,
+                }
+            }
+        }
+
+        impl #ident {
+            pub fn iter() -> impl Iterator<Item = Self> {
+                vec![ #(Self::#variant_idents,)* ].into_iter()
+            }
+        }
+    };
+    output.into()
 }
